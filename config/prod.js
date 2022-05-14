6c1e2afe7fab193d7fdc4f4869fd7c244088458c
Andrew Cheng <password>
2022-05-14 12:04:44 -0700

    updated keys

--- config/prod.js
+++ config/prod.js
@@ -4,6 +4,7 @@ module.exports = {
     mongoURI: process.env.MONGO_URI,
     cookieKey: process.env.COOKIE_KEY,
     stripeApiSecret: process.env.STRIPE_SECRET_KEY,
+    stripeEndpointSecret: process.env.STRIPE_ENDPOINT_SECRET,
     sendGridKey: process.env.SEND_GRID_KEY,
     redirectDomain: process.env.REDIRECT_DOMAIN
 };
